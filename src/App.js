3e986a8eee713dfc4f23532f566eb68cd74ad559
Rekha Bhandari <rekhabhandaree77@gmail.com>
2021-05-31 10:28:48 +0545

    Last commit

--- src/App.js
+++ src/App.js
@@ -8,17 +8,22 @@ export default function App() {
          setResult(result.concat(e.target.name));
      }
 
-     const Clear = (e) =>{
+     const Clear = () =>{
          setResult('');
 
      }
 
-     const Back = (e) =>{
-
+     const Back = () =>{
+        setResult(result.slice(0, -1))
     }
 
-    const Equal = (e) =>{
-
+    const Equal = () =>{
+        try{
+            setResult(eval(result).toString())
+        }catch(err)
+        {
+            setResult("Error")
+        }
     }
 
     return (
@@ -27,24 +32,24 @@ export default function App() {
                     <input type='text' value={result}></input>
                 </form>
                 <div className='Keypad'>
-                    <button onClick = {Clear} id = 'clear'>Clear</button>
-                    <button onClick = {Back} id = 'back'>C</button>
-                    <button name='/' onClick = {handleClick} className='a'>&divide;</button>
+                    <button  onClick = {Clear} id = 'clear'>Clear</button>
+                    <button   onClick = {Back} id = 'back'>C</button>
+                    <button name='/' onClick = {handleClick} id='divide'>&divide;</button>
                     <button name='7' onClick = {handleClick}>7</button>
                     <button name='8' onClick = {handleClick}>8</button>
                     <button name='9' onClick = {handleClick}>9</button>
-                    <button name='*' onClick = {handleClick} className='a'>&times;</button>
+                    <button name='*' onClick = {handleClick} id='times'>&times;</button>
                     <button name='4' onClick = {handleClick}>4</button>
                     <button name='5' onClick = {handleClick}>5</button>
                     <button name='6' onClick = {handleClick}>6</button>
-                    <button name='-' onClick = {handleClick} className='a'>&ndash;</button>
+                    <button name='-' onClick = {handleClick} id='minus'>&ndash;</button>
                     <button name='1' onClick = {handleClick}>1</button>
                     <button name='2' onClick = {handleClick}>2</button>
                     <button name='3' onClick = {handleClick}>3</button>
-                    <button name='+' onClick = {handleClick} className='a'>+</button>
+                    <button name='+' onClick = {handleClick} id='plus'>+</button>
                     <button name='0' onClick = {handleClick}>0</button>
                     <button name='.' onClick = {handleClick}>.</button>
-                    <button onClick = {Equal} id = 'equal'>=</button>
+                    <button  onClick = {Equal} id = 'equal'>=</button>
                 </div>
             </div>
     )
